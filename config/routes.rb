d12b8653e61ce24e91d1ed7ebcd269321e00a8aa
NondasNF <nondasnoronha@gmail.com>
2022-05-02 02:54:02 -0300

    add application style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'albums#index'
-  resources :albums
+  resources :albums do
+    member do
+      delete :delete_attached_image
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
